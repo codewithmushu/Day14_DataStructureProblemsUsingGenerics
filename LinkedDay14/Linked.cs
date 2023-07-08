89c71bcad0bab23f3c570eaf3c6f48e5845448e1
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-07-08 17:19:35 +0530

    [Muskan] Add : Created simple linked list for 56, 30, 70.

---
+++ LinkedDay14/Linked.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedDay14
+{
+    public class Linked
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data +" ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedDay14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedDay14
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please choose the option below:");
+                Console.WriteLine("1: UC1:Simple Linked List");
+                int choice =Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Linked linked = new Linked();
+                        linked.Add(56);
+                        linked.Add(30);
+                        linked.Add(70);
+                        linked.Display();
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
